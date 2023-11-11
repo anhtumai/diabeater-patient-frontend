3db6b2bfd0ffdf1b26b74d24c550c9f47c060acb
anhtumai <tu.mai@outlook.com>
2023-11-11 13:55:54 +0200

    remove CardOne CardTwo ...

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -45,24 +45,23 @@ const ECommerce = () => {
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         <BloodLevelCard />
-        <CardOne />
-        <CardTwo />
-        <CardThree />
-        <CardFour />
+        <BloodLevelCard />
+        <BloodLevelCard />
+        <BloodLevelCard />
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
-        <MapOne />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
-        </div>
         <ChatCard />
       </div>
     </>
   );
 };
 
+//<ChartOne />
+//    <ChartTwo />
+//       <ChartThree />
+//       <MapOne />
+//<div className="col-span-12 xl:col-span-8">
+//  <TableOne />
+//</div>;
 export default ECommerce;
